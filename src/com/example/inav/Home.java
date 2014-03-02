333f98c518e18efd1a7a258333e2f85f3e31c467
Robert Brown <rbrown5@mix.wvu.edu>
2014-03-02 18:22:54 -0500

    Should be a better ui start cleaner code

---
+++ src/com/example/inav/Home.java
@@ -0,0 +1,24 @@
+package com.example.inav;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Home extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//Remove title bar
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.activity_home);
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+}
---
+++ src/com/example/inav/Splash.java
@@ -0,0 +1,32 @@
+package com.example.inav;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.os.Handler;
+import android.content.Intent;
+
+public class Splash extends Activity {
+	private static int STO = 4500;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//Remove title bar
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.activity_splash);
+		
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				Intent i = new Intent(Splash.this, Home.class);
+				startActivity(i);
+				finish();
+			}
+		}, STO);
+	}
+
+
+
+}
