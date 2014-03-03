6731bfe6c1ffca1b62dce617d8ae87c5b7b5f1b2
Billy Hardy <billy92@gmail.com>
2014-03-02 23:28:28 -0500

    Testing git in eclipse

--- src/com/example/inav/Home.java
+++ src/com/example/inav/Home.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 
 public class Home extends Activity {
 		
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
